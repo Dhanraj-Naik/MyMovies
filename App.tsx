0cef698a149fad4cfcdf48fba15e575d9ba0e393
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2024-04-17 20:24:37 +0530

    navigation package added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/pages/home/Home';
+import DetailsScreen from './src/pages/details/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>HELLO DJ</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Overview' }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
 export default App;
